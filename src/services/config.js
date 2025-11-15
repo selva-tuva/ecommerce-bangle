6a4cc5dfc03916e2a14c3ccf7000de60bbd6d019
selva-tuva <selva@tuvalabs.com>
2025-11-15 15:04:35 +0530

    Own config added

--- src/services/config.js
+++ src/services/config.js
@@ -1,11 +1,22 @@
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID
+// };
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyCPyB_drzpRvyKidEJDUb7HaC1zCZPDxTQ",
+  authDomain: "my-ecommerce-bangle.firebaseapp.com",
+  projectId: "my-ecommerce-bangle",
+  databaseURL: "https://my-ecommerce-bangle-default-rtdb.firebaseio.com",
+  storageBucket: "my-ecommerce-bangle.firebasestorage.app",
+  messagingSenderId: "637055937002",
+  appId: "1:637055937002:web:1fe786cbc968d285616a93",
+  measurementId: "G-MZ0NHGCTE9"
 };
 
 export default firebaseConfig;
